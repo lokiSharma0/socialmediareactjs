4a6a12007b4502af5077089807479520a0afa6c1
luffyJoyboy <lorenzsharma@gmail.com>
2024-07-26 21:32:49 +1000

    server name change

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-const express=require("express")
-const app=express()
-const cors=require("cors")
-const db=require("./databaseconn")
-const userroute=require("./routes/users")
-const authroute=require("./routes/auth")
-const postrouts=require("./routes/post")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./databaseconn");
+const userroute = require("./routes/users");
+const authroute = require("./routes/auth");
+const postrouts = require("./routes/post");
 
-//middelwere
-app.use(cors())
-app.use(express.json())
-app.use("/api/users",userroute)
-app.use("/api/auth",authroute)
-app.use("/api/post",postrouts)
+//middelweres
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userroute);
+app.use("/api/auth", authroute);
+app.use("/api/post", postrouts);
 
 //listening on port 5000
-app.listen(5000,(req,res)=>{console.log("Server connected to port 5000");})
+app.listen(6000, (req, res) => {
+  console.log("Server connected to port 6000");
+});
